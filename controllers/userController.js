b7e715d72e3c3ccd42b01ac44229d84cfae4eee2
Davette Bryan <davette@Davettes-MacBook-Air.local>
2021-06-13 19:38:39 -0700

    Sets cookie when creating new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,6 +19,7 @@ userController.createUser = (req, res, next) => {
       //create new user with username and pass
       const newUser = await models.User.create(user);
       //check on this later
+      res.locals.verified = newUser._id;
       res.locals.user = newUser._id;
       next();
     });
